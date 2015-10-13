0c30d9e39750568b50c3251b045c4c19cf4219f3
Seth McGinnis <mcginnis@ucar.edu>
2015-10-13 17:05:18 -0600

    Added a template file for functions.

---
+++ template.R
@@ -0,0 +1,27 @@
+##' One-line sentence description of the function.
+##'
+##' This is the description of the function.  It's the first full
+##' paragraph, and should briefly describe what the function does.
+##'
+##' Third and subsequent paragraphs are details: a long section shown
+##' after the argument that goes into detail about how the function
+##' works.
+##'
+##' @param a A description of argument A.
+##'
+##' @param b A description of argument B.
+##'
+##' @return The return value of the function.
+##'
+##' @examples
+##' x <- "example code"
+##' y <- "more example code"
+##' paste(x, y)
+##' 
+##' @export
+
+template <- function(a, b){
+    dummy <- a + b
+    return(dummy)
+}
+
