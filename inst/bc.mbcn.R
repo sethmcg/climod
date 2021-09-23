8ccda95a1020e36334d869b29779a92aadeb9afa
Seth McGinnis <mcginnis@ucar.edu>
2021-09-23 17:45:46 -0600

    Fixed time misalignment bug in bc.mbcn. Better alignment of cur with obs, correction done in 38-yr chunks to better preserves trends.

--- inst/bc.mbcn.R
+++ inst/bc.mbcn.R
@@ -6,10 +6,13 @@ suppressMessages(library(ncdf4))
 suppressMessages(library(zoo))
 suppressMessages(library(MBC))
 
+## TODO: add option to transform tmax/tmin to tmed = mean(tmax,tmin)
+## and dtr = diff(tmax,tmin) (and undo it on readout)
+
+## TODO: add option to declare scaling from command-line
+
 ## "ratio sequence": variables that are adjusted with multiplicative
 ## scaling instead of additive shifting.
-
-## Note: arguably, we should transform tmax/tmin to tmed/dtr
 
 rvar <- c(huss=TRUE, prec=TRUE, rsds=FALSE, tmax=FALSE, tmin=FALSE,
           uas=FALSE, vas=FALSE)
@@ -20,18 +23,22 @@ scaling <- c(huss=1000, prec=1, rsds=1/10, tmax=1, tmin=1, uas=10, vas=10)
 
 #######
 
+## Call as: Rscript bc.mbcn.R varlist obs cur fut cout fout [seed]
+
 # For testing
-args <- c("huss,prec,rsds,tmax,tmin",
-          "testin/obs/huss.obs.DAYMET-ns.day.NAM-44i/slice/x150/data.x150.y040.nc",
-          "testin/raw/huss.hist.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.raw/slice/x150/data.x150.y040.nc",
-          "testin/raw/huss.rcp85.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.raw/slice/x150/data.x150.y040.nc",
-          "testout/mbcn-DAYMET-ns/huss.hist.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.mbcn-DAYMET-ns/slice/x150/data.x150.y040.nc",
-          "testout/mbcn-DAYMET-ns/huss.rcp85.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.mbcn-DAYMET-ns/slice/x150/data.x150.y040.nc"
-)
+args <-c("huss,prec,rsds,tmax,tmin,uas,vas",
+         "data/obs/huss.obs.gridMET.day.NAM-44i.nc",
+         "data/raw/huss.hist.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.raw.nc",
+         "data/raw/huss.rcp85.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.raw.nc",
+         "data/fix/huss.hist.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.mbcn-gridMET.nc",
+         "data/fix/huss.rcp85.MPI-ESM-MR.CRCM5-UQAM.day.NAM-44i.mbcn-gridMET.nc"
+         )
+verbose <- TRUE
 
-## comment out this line for testing
+
+## comment out these lines for testing
 args <- commandArgs(trailingOnly=TRUE)
-
+verbose <- FALSE
 
 
 vars <- unlist(strsplit(args[1], ","))
@@ -133,34 +140,126 @@ timefiles <- sapply(infiles, `[`, 1)
 names(timefiles) <- names(infiles)
 timefiles <- gsub("slice/.*nc", "time.nc", timefiles)
 
+
+## get times and convert to fractional year
+
 times <- lapply(timefiles, gettime)
 
+atime <- lapply(times, alignepochs, "days since 1900-01-01")
+years <- lapply(atime, function(x){x/yearlength(x)+1900})
+yfrac <- lapply(years, '%%', 1)
 
+
+
+## construct indexing for ~monthly windowing, plus an outer window of
+## 2 months on either side (3 months total) for better statistics.
 
 N <- 12
+mbounds <- seq(0, 1, length=N+1)
 
-monwin <- lapply(times, cslice, num=N, ratio=2, split=FALSE)
+mbins <- lapply(yfrac, .bincode, breaks=mbounds, include.lowest=TRUE) 
 
-bcdata <- renest(mapply(slice, indata, monwin, MoreArgs=list(outer=TRUE), SIMPLIFY=FALSE))
+innerwindow <- lapply(mbins, function(x){outer(x, seq(N), '==')})
+
+before <- c(N, seq(N-1))
+after  <- c(seq(N-1)+1, 1)
+
+outerwindow <- lapply(innerwindow, function(x){x[,before] | x | x[,after]})
 
 
+## TODO: automate and parameterize splitting model dataq into chunks
+## that match the obs data in size (and start/end, for 'current'.)
+
+## indexing to split data for BC into 4 x 38-year chunks, plus a
+## 38-year chunk ("current") matching the obs period
+
+period <- c("late20c", "turncty", "mid21c", "end21c", "current")
+ystart <- c(1949, 1987, 2025, 2063, 1979)
+yend   <- c(1986, 2024, 2062, 2101, 2017)
+names(ystart) <- names(yend) <- period
 
 
-fits <- list()
+## concatenate model data, then re-split
 
-cat(date(),"\n")
-for(i in 1:N){
-    cat(i, "of", N, ": ")
-    bcme <- bcdata[[i]]
-    fits[[i]] <- MBCn(bcme$obs, bcme$cur, bcme$fut, ratio.seq=rvar, trace=0.1) 
+y <- c(years$cur, years$fut)
+
+iper <- mapply(function(a, b){a<y & y<=b}, ystart, yend)
+adata <- rbind(indata$cur, indata$fut)
+
+## lapply is very slow; use loop instead
+perdata <- list()
+for(p in period){
+    perdata[[p]] <- adata[iper[,p],]
 }
-cat(date(),"\n")
+perdata$obs <- indata$obs
+                           
 
-rfit <- renest(fits)
+### quick check:
+# par(mfrow=c(6,1))
+# for(p in period){
+#     plot(y[iper[,p]], perdata[[p]][,"tmax"], xlim=c(1948, 2102), pch='.', main=p)
+# }
+# plot(years$obs, perdata$obs[,"tmax"], xlim=c(1948, 2102), pch='.', main="obs")
+
+
+## and do the same for the windowing indexes
+
+ainnerwindow <- rbind(innerwindow$cur, innerwindow$fut)
+aouterwindow <- rbind(outerwindow$cur, outerwindow$fut)
+
+pinnerwin <- list()
+pouterwin <- list()
+for(p in period){
+    pinnerwin[[p]] <- ainnerwindow[iper[,p],]
+    pouterwin[[p]] <- aouterwindow[iper[,p],]
+}
+
+pinnerwin$obs <- innerwindow$obs
+pouterwin$obs <- outerwindow$obs
+
+
+
+## empty arrays to hold  results
+bcdata <- lapply(perdata, function(x){x+NA})
+bcdata$obs <- bcdata$current <- NULL
+
+
+### then loop over both period and window
+
+if(verbose) cat(date(),"\n")
+for(p in head(period, -1)){
+    ocf <- c("obs","current",p)
+    if(verbose) cat(p, ":\n")
+    for(i in 1:N){
+        if(verbose) cat(i, "of", N, ": ")
+        bcme <- mapply(function(D, mask){D[mask[,i],]},
+                       perdata[ocf], pouterwin[ocf], SIMPLIFY=FALSE)
+        result <- suppressWarnings(
+            MBCn(bcme$obs, bcme$current, bcme[[p]],
+                 ratio.seq=rvar, trace=0.1, silent=!verbose)
+            )
+
+        # indexes of inner window for month i, period p
+        idex <- which(pinnerwin[[p]][,i])
+
+        # indexes of inner window within outer window for month i, period p
+        odex <- which(pinnerwin[[p]][pouterwin[[p]][,i],i])
+
+        bcdata[[p]][idex,] <- result$mhat.p[odex,]
+    }
+}
+if(verbose) cat(date(),"\n")
+
+# run time for bc loop: 105 seconds on my mac 
+
+
+## combine 4x38-year chunks back into cur & fut
+odata <- do.call(rbind, bcdata)
 
 outdata <- list()
-outdata$cur <- unslice(rfit$mhat.c, monwin$cur, template=indata$cur)
-outdata$fut <- unslice(rfit$mhat.p, monwin$fut, template=indata$fut)
+outdata$cur <- head(odata, nrow(indata$cur))
+outdata$fut <- tail(odata, nrow(indata$fut))
+
 
 
 ## write results out to netcdf file
@@ -180,4 +279,3 @@ outgest <- function(ifiles, ofiles, datarray){
 for(i in c("cur","fut")){
     outgest(infiles[[i]], outfiles[[i]], outdata[[i]])
 }
-
